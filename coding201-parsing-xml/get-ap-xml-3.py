4e9b84907b65b5d123df49e26060d5d48b2e1882
jerbrune <jerbrune@gmail.com>
2020-05-14 19:11:47 +0200

    com

--- coding201-parsing-xml/get-ap-xml-3.py
+++ coding201-parsing-xml/get-ap-xml-3.py
@@ -1,5 +1,8 @@
 from urllib.request import Request, urlopen
 import xml.dom.minidom 
+import ssl 
+ssl._create_default_https_context = ssl._create_unverified_context
+
 req = Request('https://msesandbox.cisco.com/api/contextaware/v1/maps/info/DevNetCampus/DevNetBuilding/DevNetZone')
 req.add_header('Authorization', 'Basic bGVhcm5pbmc6bGVhcm5pbmc==')
 response = urlopen(req)
